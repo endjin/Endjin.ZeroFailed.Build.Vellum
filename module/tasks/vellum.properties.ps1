0de0122c1fb62b60b685f3d45d422bee0c4cd3af
James Dawson <james.dawson@endjin.com>
2025-07-31 21:53:28 +0100

    Improve generation of Vite-related configuration templates (#1)

--- module/tasks/vellum.properties.ps1
+++ module/tasks/vellum.properties.ps1
@@ -11,8 +11,8 @@ $VellumGitHubRepo = 'endjin/Endjin.StaticSiteGen'
 # Synopsis: The name of the NuGet package that distributes the Vellum .NET global tool
 $VellumGlobalToolPackageName = 'vellum'
 
-# Synopsis: The version of the vellum cli to use; Defaults to the latest stable release
-$vellumVersion = ''         # when empty, the 'Latest' release will be installed
+# Synopsis: The version of the vellum cli to use. When empty the latest stable release will be used.
+$VellumVersion = ''
 
 # Synopsis: The name of the folder containing the site's theming assets
 $SiteName = 'undefined-site-name'
@@ -20,22 +20,22 @@ $SiteName = 'undefined-site-name'
 # Synopsis: The root path containing the web site source files
 $SiteBasePath = '.'
 
-# Synopsis: The URL of the site's git repository (e.g. 'https://github.com/myorg/mysite'); Used to populate configuration for Vite package.json file
+# Synopsis: The URL of the site's git repository (e.g. 'https://github.com/myorg/mysite'); Used to populate configuration for Vite's package.json file
 $SiteRepositoryUrl = 'https://undefined-site-repo-fqdn'
 
-# Synopsis: The path where the vellum cli outputs are generated; Defaults to './.output'
+# Synopsis: The path where the vellum-related artefacts are stored (e.g. cli tool, Vite configuration, intermediate & output folders)
 $VellumBasePath = Join-Path $here '.vellum'
 
-# Synopsis: The path where the vellum cli outputs are generated; Defaults to './.output'
+# Synopsis: The path where the vellum cli outputs are generated
 $StaticSiteOutDir = Join-Path $VellumBasePath '.output'
 
-# Synopsis: The path where the final Vite-optimised static site is generated; Defaults to './.dist'
+# Synopsis: The path where the final Vite-optimised static site is generated
 $DistDir = Join-Path $VellumBasePath '.dist'
 
-# Synopsis: When true, enables the preview mode and does not run the vite compilation process
+# Synopsis: When true, enables the preview mode and does not run the Vite compilation process
 $Preview ??= $false
 
-# Synopsis: When true, enables the watch mode which causes the build to wait indefinitely and reload the site when file changes are detected
+# Synopsis: When true, enables the watch mode which causes the build to wait indefinitely and regenerate the site when file changes are detected
 $Watch ??= $false
 
 # Synopsis: The GitHub token used to obtain the release artifact for the vellum cli global tool
---
+++ module/templates/vite.config.template.js
@@ -0,0 +1,27 @@
+// vite.config.js
+// https://vitejs.dev/config
+import { globSync } from "glob"
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const __outputDir = '#{OUTPUT_DIR}#'
+const __distDir = '#{DIST_DIR}#'
+
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import']
+      },
+    }
+  },
+  root: __outputDir,
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 600,
+    outDir: __distDir,
+    rollupOptions: {
+      input: globSync([`${resolve(__outputDir)}/**/*.html`])
+    }
+  }
+})
